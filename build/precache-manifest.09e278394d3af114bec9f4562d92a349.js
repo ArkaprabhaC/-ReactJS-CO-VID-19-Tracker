f266817465b6e70c69cf40ca616f011e23cbf5d0
ArkaprabhaC <arkaprabha.chatterjee31@gmail.com>
2020-04-11 18:09:26 +0530

    Added Node Server to host React App and using production build

---
+++ build/precache-manifest.09e278394d3af114bec9f4562d92a349.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c8b060f0f4fedaa8f12a3cb26162a73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f7c15611c3bc5898134",
+    "url": "/static/css/main.ea5174b5.chunk.css"
+  },
+  {
+    "revision": "e5a8c0fc1ccb5395be67",
+    "url": "/static/js/2.e8232a34.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/static/js/2.e8232a34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f7c15611c3bc5898134",
+    "url": "/static/js/main.5594b07a.chunk.js"
+  },
+  {
+    "revision": "63f4e8b58fac8513d449",
+    "url": "/static/js/runtime-main.310cdbaa.js"
+  }
+]);
